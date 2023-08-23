2685f0e1290ca086691eac88b8672b9525d92f67
Alfito Dimas Prasetyo <alfito.in08@gmail.com>
2023-08-23 10:29:38 +0700

    Bottom Nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_boring/screen/history.dart';
+import 'package:flutter_boring/screen/home.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -23,23 +25,51 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
           useMaterial3: true,
         ),
-        home: const Home());
+        home: MyBottomTabBar(),);
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
-
+class MyBottomTabBar extends StatefulWidget {
   @override
-  State<Home> createState() => _HomeState();
+  _MyBottomTabBarState createState() => _MyBottomTabBarState();
 }
 
-class _HomeState extends State<Home> {
+class _MyBottomTabBarState extends State<MyBottomTabBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Home(),
+    History(),
+ 
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Boring"),
+        title: Text('Boring Application'),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Histori',
+          ),
+          
+        ],
       ),
     );
   }
