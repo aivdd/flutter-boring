edb4c9af2129c609471018271d591af7b5476dff
David Vid <davidyusufrahmanto@gmail.com>
2023-08-28 10:39:19 +0700

    scaffold

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -10,44 +10,46 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 200,
-          color: Colors.blue[900], // Warna biru tua
-          child: Align(
-            alignment: Alignment.centerLeft, // Penempatan di atas dan kiri
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  top: 20.0, left: 16.0), // Menambah jarak atas
-              child: Text(
-                'Hallo, XYZ',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 200,
+            color: Colors.blue[900], // Warna biru tua
+            child: Align(
+              alignment: Alignment.centerLeft, // Penempatan di atas dan kiri
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 20.0, left: 16.0), // Menambah jarak atas
+                child: Text(
+                  'Hallo, XYZ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-              top: 10.0, left: 5.0, right: 5.0), // Menambah margin atas
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Cari...',
-              prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20.0),
+          Container(
+            margin: EdgeInsets.only(
+                top: 10.0, left: 5.0, right: 5.0), // Menambah margin atas
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
